0b5d0b87b05a7a89e23dd0ca9a0f1f4e634a9207
yituodab <a111664@qq.com>
2024-03-29 20:48:18 +0800

    1

--- src/main/java/com/eihei/wand/items/TNTWandItem.java
+++ src/main/java/com/eihei/wand/items/TNTWandItem.java
@@ -4,8 +4,10 @@ import org.apache.commons.compress.archivers.dump.DumpArchiveEntry.TYPE;
 
 import com.eihei.wand.utils.EntityUtil;
 import com.ibm.icu.impl.TZDBTimeZoneNames;
+import com.ibm.icu.util.ULocale.Type;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -21,13 +23,17 @@ public class TNTWandItem extends Item {
     public TNTWandItem(Properties properties) {
         super(properties);
     }
+    public static final EntityDataAccessor<String> TAGGET = SynchedEntityData.defineId(PrimedTnt.class, EntityDataSerializers.STRING);
+    public static final EntityDataAccessor<Boolean> TAGSET = SynchedEntityData.defineId(PrimedTnt.class, EntityDataSerializers.BOOLEAN);
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand useHand) {
             PrimedTnt tnt = new PrimedTnt(level, player.getX(), player.getY() + 2, player.getZ(), player);
             tnt.setFuse(40);
             CompoundTag tag = new CompoundTag();
             tag.put("Motion", EntityUtil.newDoubleList(1.0, 1.0, 0.0));
-            tnt.getEntityData().set(SynchedEntityData.defineId(PrimedTnt.class, EntityDataSerializers.COMPOUND_TAG), tag);
+            tnt.getEntityData().set(TAGGET, "tag");
+            tnt.getEntityData().set(TAGSET, false);
+            tnt.getEntityData().set(TAGGET, tag.getString("Motion"));
             level.addFreshEntity(tnt);
         return InteractionResultHolder.success(player.getItemInHand(useHand));
     }
