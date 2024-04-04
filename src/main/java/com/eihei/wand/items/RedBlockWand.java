daeb2fc4b255b6c802bd0ae183d403df84022acd
yituodab <a111664@qq.com>
2024-04-04 18:46:59 +0800

    red

--- src/main/java/com/eihei/wand/items/RedBlockWand.java
+++ src/main/java/com/eihei/wand/items/RedBlockWand.java
@@ -1,5 +1,7 @@
 package com.eihei.wand.items;
 
+import com.eihei.wand.tool.Ways;
+
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleDescription;
 import net.minecraft.client.particle.ParticleRenderType;
@@ -8,6 +10,7 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -16,6 +19,7 @@ import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.NoiseChunk.BlockStateFiller;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
@@ -32,7 +36,7 @@ public class RedBlockWand extends Item{
             double x = location.x-player.getX();
             double y = location.y-player.getY();
             double z = location.z-player.getZ();
-            Vec3 vec = new Vec3(player.getX(), player.getY(), player.getZ());
+            Vec3 vec = new Vec3(player.getX(), player.getY()+1, player.getZ());
             Vec3 Vec = new Vec3(x,y,z);
             double Line = vec.distanceTo(Vec);
             double X = x/Line;
@@ -40,11 +44,15 @@ public class RedBlockWand extends Item{
             double Z = z/Line;
             Vec3 jia = new Vec3(X,Y,Z);
             for(double line = 0;line<Line;line = line +1,vec = vec.add(jia)){
-                level.addParticle(ParticleTypes.FALLING_OBSIDIAN_TEAR, vec.x, vec.y, vec.z, vec.x, vec.y, vec.z);
+                level.addParticle(ParticleTypes.LAVA, vec.x, vec.y, vec.z, vec.x, vec.y, vec.z);
             }
-            BlockState air = Blocks.AIR.defaultBlockState();
-            level.setBlock(new BlockPos(location), air, 1);
             BlockPos Location = new BlockPos(location.x, location.y, location.z);
+            level.setBlock(Location, Blocks.AIR.defaultBlockState(), 0);
+            Entity entity = Ways.getPointedEntity(player, Line);
+            if(entity instanceof Player play){
+                float health = play.getHealth();
+                play.setHealth(health - 5);
+            }
             Block block = level.getBlockState(Location).getBlock();
             if(block.equals(Blocks.AMETHYST_BLOCK)){
             }
