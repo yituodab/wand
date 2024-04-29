982370b534bc48865536cfc312e24d51bc26daca
yituodab <a111664@qq.com>
2024-04-29 20:00:54 +0800

    command

--- src/main/java/com/eihei/wand/items/WandItem.java
+++ src/main/java/com/eihei/wand/items/WandItem.java
@@ -8,11 +8,14 @@ import com.eihei.wand.utils.EntityUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -23,19 +26,22 @@ public class WandItem extends Item{
   public WandItem(Properties properties) {
     super(properties);
   }
+    // TODO Auto-generated method stub
   @Override
-  public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand useHand) {
-    BlockPos location = new BlockPos(Pos.main(5, player));
-    Block block = level.getBlockState(location).getBlock();
-    ItemStack inHand = player.getItemInHand(useHand);
-    ItemStack item;
-    if(block.equals(Blocks.REDSTONE_BLOCK)){
+  public InteractionResult useOn(UseOnContext use) {
+    Level level = use.getLevel();
+    Player player = use.getPlayer();
+    BlockPos pos = use.getClickedPos();
+    Block block = level.getBlockState(pos).getBlock();
+    ItemStack inHand = player.getItemInHand(use.getHand());
+    ItemStack item = new ItemStack(Items.AIR);
+    if(block.equals(Blocks.REDSTONE_BLOCK))item = new ItemStack(ModItems.RED_BLOCK_WAND.get());
+    if(block.equals(Blocks.REDSTONE_BLOCK)||block.equals(Blocks.NETHERITE_BLOCK)){
       player.getInventory().removeItem(inHand);
-    	item = new ItemStack(ModItems.RED_BLOCK_WAND.get());
       player.addItem(item);
     }
     // TODO Auto-generated method stub
-    return super.use(level, player, useHand);
+    return InteractionResult.SUCCESS;
   }
   public static final int MAX_STACK_SIZE = 1;
   
