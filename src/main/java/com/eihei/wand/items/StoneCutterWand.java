3c3c53ca00bc6b17e94df0e1fb83e16d4eeb9def
yituodab <a111664@qq.com>
2024-04-29 20:06:25 +0800

    1

--- src/main/java/com/eihei/wand/items/StoneCutterWand.java
+++ src/main/java/com/eihei/wand/items/StoneCutterWand.java
@@ -2,10 +2,12 @@ package com.eihei.wand.items;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.HitResult;
@@ -16,11 +18,11 @@ public class StoneCutterWand extends Item {
         super(p_41383_);
     }
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand useHand) {
-            HitResult hitResult = player.pick(5, 0, false);
-            Vec3 location = hitResult.getLocation();
-            level.setBlock(new BlockPos(location), Blocks.AIR.defaultBlockState(), 0);
-            return InteractionResultHolder.success(player.getItemInHand(useHand));
+    public InteractionResult useOn(UseOnContext use){
+        BlockPos pos = use.getClickedPos();
+        Level level = use.getLevel();
+        level.setBlock(pos, Blocks.AIR.defaultBlockState(), 0);
+        return InteractionResult.SUCCESS;
     }
     public static final int MAX_STACK_SIZE = 1;
 }
