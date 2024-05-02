68ec01c0d4db4494a260391bd5e520072447605a
yituodab <a111664@qq.com>
2024-05-02 16:08:23 +0800

    tnt

--- src/main/java/com/eihei/wand/items/TNTWandItem.java
+++ src/main/java/com/eihei/wand/items/TNTWandItem.java
@@ -1,27 +1,14 @@
 package com.eihei.wand.items;
 
-import org.apache.commons.compress.archivers.dump.DumpArchiveEntry.TYPE;
-
 import com.eihei.wand.utils.EntityUtil;
-import com.ibm.icu.impl.TZDBTimeZoneNames;
-import com.ibm.icu.util.ULocale.Type;
-
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.Arrow;
-import net.minecraft.world.entity.projectile.Snowball;
-import net.minecraft.world.entity.projectile.ThrowableProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SnowballItem;
 import net.minecraft.world.level.Level;
 
 public class TNTWandItem extends Item {
@@ -34,9 +21,7 @@ public class TNTWandItem extends Item {
             tnt.setFuse(40);
             CompoundTag tag = new CompoundTag();
             tag.put("Motion", EntityUtil.newDoubleList(5.0, 1.0, 0.0));
-            tnt.save(tag);
-            tnt.load(tag);
-            tnt.deserializeNBT(tag);
+            tnt.getEntityData().set(tnt.getEntityData().defineId(PrimedTnt.class, EntityDataSerializers.COMPOUND_TAG), tag);
             level.addFreshEntity(tnt);
         return InteractionResultHolder.success(player.getItemInHand(useHand));
     }
